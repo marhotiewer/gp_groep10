0452f7e3fee4e851f2fc1a0fa4e05144aa2f0fef
marhotiewer <marhotiewer@protonmail.com>
2021-04-06 09:39:39 +0200

    Added throttle control

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <wiringPi.h>
+#include <softPwm.h>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -12,6 +13,10 @@ using json = nlohmann::json;
 #define SERVO_PWM_CLOCK	1920
 #define SERVO_PWM_RANGE	200
 
+#define MOTOR_PWM		7
+#define DIRECTIONAL_2	6
+#define DIRECTIONAL_1	5
+
 int main(int argc, char* argv[]) {
 	httplib::Server svr;
     wiringPiSetup();
@@ -22,6 +27,13 @@ int main(int argc, char* argv[]) {
     pwmSetClock(SERVO_PWM_CLOCK);
     pwmSetRange(SERVO_PWM_RANGE);
     pwmWrite(SERVO_STR_PWM, 13);
+	
+	// Prepare software pwm for throttle and set it forward
+	softPwmCreate(MOTOR_PWM, 0, 100);
+	pinMode(DIRECTIONAL_1, OUTPUT);
+	pinMode(DIRECTIONAL_2, OUTPUT);
+	pinWrite(DIRECTIONAL_1, HIGH);
+	pinWrite(DIRECTIONAL_1, LOW);
     
 	// Bind webserver endpoints
 	svr.Get("/", [](const httplib::Request &req, httplib::Response &res) {
@@ -36,7 +48,12 @@ int main(int argc, char* argv[]) {
         pwmWrite(SERVO_STR_PWM, data["angle"]);
         res.set_content(req.body, "application/json");
     });
-	
+	svr.Post("/throttle", [](const httplib::Request &req, httplib::Response &res) {
+        std::cout << "(" << req.remote_addr << ") 200 OK: " << req.body << std::endl;
+		auto data = json::parse(req.body);
+        softPwmWrite(MOTOR_PWM, data["speed"]);
+        res.set_content(req.body, "application/json");
+    });
 	// Start listening on port 80, this will block the thread
     svr.listen("0.0.0.0", 80);
     return 0;
