fd203863de08ef6651e562e8bb3d76e5fc4190cc
KyrillW <KyrillW@hotmail.com>
2021-04-07 10:45:42 +0200

    sr04 opnieuw

--- sr04.cpp
+++ sr04.cpp
@@ -6,28 +6,27 @@ void sr04(){
 	delay(1);
 	digitalWrite(5, LOW);
 
-    unsigned int start = 0;
+    unsigned int begin = 0;
     unsigned int end = 0;
 
 	while(digitalRead(4) == LOW){
-        start = millis();
+        begin = micros();
 	}
 	while(digitalRead(4) == HIGH){
-        end = millis();
+        end = micros();
 	}
 	
-	std::cout << "Afstand: " << end - begin * 17165000 << "cm\n";
+	std::cout << "Afstand: " << ((end - begin) / 2) / 29 << "cm\n";
 }
 
 int main(int argc, char* argv[]) {
-    wiringPiSetup();
-    pinMode(5, OUTPUT);
-    pinMode(4, INPUT);
-    pullUpDnControl(4, PUD_DOWN);
-
-    for(;;) {
+	wiringPiSetup();
+	pinMode(5, OUTPUT);
+	pinMode(4, INPUT);
+	pullUpDnControl(4, PUD_DOWN);
+	
+	for(;;) {
 		sr04();
 		delay(500);
 	}
 }
-
