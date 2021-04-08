afb9c447c6492930db13e34bdc9d32931a4810e6
marhotiewer <marhotiewer@protonmail.com>
2021-04-08 19:55:20 +0200

    Added comments

--- main.cpp
+++ main.cpp
@@ -23,19 +23,22 @@ using json = nlohmann::json;
 #define	SPEAKER			2
 
 int read_sr04() {
+	// Send a short pulse to the sensor to have it measure the data and send us it
 	digitalWrite(SR04_WRITE, HIGH);
 	delay(1);
 	digitalWrite(SR04_WRITE, LOW);
 
     unsigned int begin = 0;
     unsigned int end = 0;
-
+	
+	// Measure the amount of time the signal is low for
 	while(digitalRead(SR04_READ) == LOW){
         begin = micros();
 	}
 	while(digitalRead(SR04_READ) == HIGH){
         end = micros();
 	}
+	// This magic equation will give us the distance in cm from the amount of time SR04_READ was LOW
 	return ((end - begin) / 2) / 29;
 }
 
@@ -43,6 +46,7 @@ void get_distance(int &distance) {
 	for(;;){
 		distance = read_sr04();
 		
+		// Get the distance from an object and depending on the result beep the speaker
 		if(distance < 4) {
 			digitalWrite(SPEAKER, HIGH);
 			delay(50);
@@ -75,39 +79,48 @@ void get_distance(int &distance) {
 }
 
 int main(int argc, char* argv[]) {
+	// Do some initialization
 	httplib::Server svr;
 	int distance = 0;
     wiringPiSetup();
 	
 	// Prepare steering servo and set the wheels straight
+	// Set the pulse width to 20ms for our servo model
     pinMode(SERVO_STR_PWM, PWM_OUTPUT);
     pwmSetMode(PWM_MODE_MS);
     pwmSetClock(SERVO_PWM_CLOCK);
     pwmSetRange(SERVO_PWM_RANGE);
+	// A duty cycle of 1ms to 2ms will give us 0 to 180 degrees rotation, initialize the wheels to be centered
     pwmWrite(SERVO_STR_PWM, 13);
 	
 	// Prepare software pwm for throttle and set it forward
+	// Our motor doesn't care about the pulse length, only the duty cycle, so here we'll use software pwm with a duty cycle from 0 to 100%
 	softPwmCreate(MOTOR_PWM, 0, 100);
 	pinMode(DIRECTIONAL_1, OUTPUT);
 	pinMode(DIRECTIONAL_2, OUTPUT);
+	// We can control the direction of the motor by flipping these two pins, initialize it to forward
 	digitalWrite(DIRECTIONAL_1, HIGH);
 	digitalWrite(DIRECTIONAL_2, LOW);
 	
-	// Prepare parking sensor system
+	// Prepare parking sensor
+	// Make sure the pull down resister on our input is enabled so the signal wont be undefined
 	pullUpDnControl(SR04_READ, PUD_DOWN);
     pinMode(SR04_WRITE, OUTPUT);
 	pinMode(SR04_READ, INPUT);
 	pinMode(SPEAKER, OUTPUT);
 	
+	// Start the thread for the parking sensor
 	std::thread main_thread(get_distance, std::ref(distance));
 	
 	// Bind webserver endpoints
+	// Read the webpage from disk and serve it to the user to allow control of the vehicle
 	svr.Get("/", [](const httplib::Request &req, httplib::Response &res) {
         std::ifstream ifs("control.html");
         std::string control_src((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
         res.set_content(control_src, "text/html");
 		ifs.close();
     });
+	// The data for every request gets sent in json, in the following lambda functions we decode the data and control the components
     svr.Post("/steer", [](const httplib::Request &req, httplib::Response &res) {
         std::cout << "(" << req.remote_addr << ") 200 OK: " << req.body << std::endl;
         auto data = json::parse(req.body);
@@ -118,6 +131,7 @@ int main(int argc, char* argv[]) {
         std::cout << "(" << req.remote_addr << ") 200 OK: " << req.body << std::endl;
 		auto data = json::parse(req.body);
 		
+		// If direction is true go backwards, otherwise, default to forward
 		if(data["direction"]) {
 			digitalWrite(DIRECTIONAL_1, LOW);
 			digitalWrite(DIRECTIONAL_2, HIGH);
@@ -126,12 +140,13 @@ int main(int argc, char* argv[]) {
 			digitalWrite(DIRECTIONAL_1, HIGH);
 			digitalWrite(DIRECTIONAL_2, LOW);
 		}
+		// If the parking sensor isn't within 5cm the motor will drive the wheel
 		if(distance <! 5) {
 			softPwmWrite(MOTOR_PWM, data["speed"]);
 		}
         res.set_content(req.body, "application/json");
     });
-	// Start listening on port 80, this will block the thread
+	// Start listening for incoming API requests on port 80, this will block the thread
     svr.listen("0.0.0.0", 80);
     return 0;
 }
