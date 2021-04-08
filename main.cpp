288be2244969c0bfca1968981689c4eca1e01914
marhotiewer <marhotiewer@protonmail.com>
2021-04-08 10:50:06 +0200

    Parking sensor

--- main.cpp
+++ main.cpp
@@ -13,12 +13,63 @@ using json = nlohmann::json;
 #define SERVO_PWM_CLOCK	1920
 #define SERVO_PWM_RANGE	200
 
-#define MOTOR_PWM		7
+#define MOTOR_PWM		4
 #define DIRECTIONAL_2	6
 #define DIRECTIONAL_1	5
 
+#define SR04_WRITE		12
+#define	SR04_READ		13
+
+#define	SPEAKER			2
+
+int read_sr04() {
+	digitalWrite(SR04_WRITE, HIGH);
+	delay(1);
+	digitalWrite(SR04_WRITE, LOW);
+
+    unsigned int begin = 0;
+    unsigned int end = 0;
+
+	while(digitalRead(SR04_READ) == LOW){
+        begin = micros();
+	}
+	while(digitalRead(SR04_READ) == HIGH){
+        end = micros();
+	}
+	return ((end - begin) / 2) / 29;
+}
+
+void get_distance(int &distance) {
+	for(;;){
+		distance = read_sr04();
+		
+		if(distance < 5) {
+			digitalWrite(SPEAKER, HIGH);
+			delay(25);
+			digitalWrite(SPEAKER, LOW);
+			delay(25);
+		}
+		else if(distance < 10) {
+			digitalWrite(SPEAKER, HIGH);
+			delay(50);
+			digitalWrite(SPEAKER, LOW);
+			delay(50);
+		}
+		else if(distance < 15) {
+			digitalWrite(SPEAKER, HIGH);
+			delay(100);
+			digitalWrite(SPEAKER, LOW);
+			delay(100);
+		}
+		else {
+			delay(100);
+		}
+	}
+}
+
 int main(int argc, char* argv[]) {
 	httplib::Server svr;
+	int distance = 0;
     wiringPiSetup();
 	
 	// Prepare steering servo and set the wheels straight
@@ -34,7 +85,15 @@ int main(int argc, char* argv[]) {
 	pinMode(DIRECTIONAL_2, OUTPUT);
 	digitalWrite(DIRECTIONAL_1, HIGH);
 	digitalWrite(DIRECTIONAL_2, LOW);
-    
+	
+	// Prepare parking sensor system
+	pullUpDnControl(SR04_READ, PUD_DOWN);
+    pinMode(SR04_WRITE, OUTPUT);
+	pinMode(SR04_READ, INPUT);
+	pinMode(SPEAKER, OUTPUT);
+	
+	std::thread main_thread(get_distance, std::ref(distance));
+	
 	// Bind webserver endpoints
 	svr.Get("/", [](const httplib::Request &req, httplib::Response &res) {
         std::ifstream ifs("control.html");
@@ -48,7 +107,7 @@ int main(int argc, char* argv[]) {
         pwmWrite(SERVO_STR_PWM, data["angle"]);
         res.set_content(req.body, "application/json");
     });
-	svr.Post("/throttle", [](const httplib::Request &req, httplib::Response &res) {
+	svr.Post("/throttle", [&distance](const httplib::Request &req, httplib::Response &res) {
         std::cout << "(" << req.remote_addr << ") 200 OK: " << req.body << std::endl;
 		auto data = json::parse(req.body);
 		
@@ -60,7 +119,9 @@ int main(int argc, char* argv[]) {
 			digitalWrite(DIRECTIONAL_1, HIGH);
 			digitalWrite(DIRECTIONAL_2, LOW);
 		}
-		softPwmWrite(MOTOR_PWM, data["speed"]);
+		if(distance <! 5) {
+			softPwmWrite(MOTOR_PWM, data["speed"]);
+		}
         res.set_content(req.body, "application/json");
     });
 	// Start listening on port 80, this will block the thread
